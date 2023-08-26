89818dcef1ad4befa63156351d8724d339b76965
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-27 01:02:43 +0530

    updating codes

--- src/Link.js
+++ src/Link.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { useHistory, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { db } from "./firebase";
 
 export default function Link() {
   const { code } = useParams();
-  const history = useHistory();
+  const navigate = useNavigate();
   const [url, setUrl] = useState("");
 
   useEffect(() => {
     let query = db.collection("urls").where("code", "==", code);
     query.onSnapshot((data) => {
       if (data.empty) {
-        return history.push("/");
+        return navigate.push("/");
       }
       let finalData = data.docs[0].data();
 
