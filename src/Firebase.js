a5f4603918a12e3be2bb0f1f7a2d56352c54ad6c
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-26 19:09:55 +0530

    Intial Commit

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,11 +1,13 @@
 import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
   apiKey: "AIzaSyArQ9J3WNhiaDsVBI-Sb0Y748WRlsaCOc8",
   authDomain: "link-shortner-2f466.firebaseapp.com",
   projectId: "link-shortner-2f466",
   storageBucket: "link-shortner-2f466.appspot.com",
   messagingSenderId: "876581122310",
-  appId: "1:876581122310:web:95eec1dc4c196830cc2e87",
+  appId: "1:876581122310:web:7590bae595139735cc2e87"
 };
+
 
 const app = initializeApp(firebaseConfig);
