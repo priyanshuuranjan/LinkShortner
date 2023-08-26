dc01a24bf79b607fd363c6666822f865fd3ff348
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-26 19:00:39 +0530

    Setup files

---
+++ src/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyArQ9J3WNhiaDsVBI-Sb0Y748WRlsaCOc8",
+  authDomain: "link-shortner-2f466.firebaseapp.com",
+  projectId: "link-shortner-2f466",
+  storageBucket: "link-shortner-2f466.appspot.com",
+  messagingSenderId: "876581122310",
+  appId: "1:876581122310:web:95eec1dc4c196830cc2e87"
+};
