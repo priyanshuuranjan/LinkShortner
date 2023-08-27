8ee3ad13286487edb5b885d613e0e278fdce5e8c
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-27 15:42:56 +0530

    updating codes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
-import Home from "./Home";
-import Link from "./Link";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
+import BackgroundAnimate from './BackgroundAnimate';
+import InputShortener from './InputShortener';
+import LinkResult from './LinkResult';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/l/:code" component={Link} />
-        </Routes>
-      </BrowserRouter>
+    <div className="container">
+      <InputShortener setInputValue={setInputValue} />
+      <BackgroundAnimate />
+      <LinkResult inputValue={inputValue} />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import Home from "./Home";
+// import Link from "./Link";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <BrowserRouter>
+//         <Routes>
+//           <Route exact path="/" element={<Home />} />
+//           <Route exact path="/l/:code" component={Link} />
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
